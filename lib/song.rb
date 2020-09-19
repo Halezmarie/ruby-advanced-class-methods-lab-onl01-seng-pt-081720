42f6b95705efbcb8a985d797db8692f5e391b6a5
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-19 01:34:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,14 @@ def self.find_by_name(name) #now we are finding the songs
  #grabbing element from array and returning it 
  end
 
+def self.find_or_create_by_name(name)
+  if self.find_name_name(name)
+    self.find_by_name(name)
+  else
+    self.create_by_name(name)
+end
+
+
 end 
 
 
