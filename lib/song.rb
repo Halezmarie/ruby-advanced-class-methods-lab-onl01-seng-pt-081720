6783a431f2b965cf8c731c62a155f85e7953b957
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-19 02:07:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -63,9 +63,13 @@ def self.new_from_filename(filename)
   # split_filename = filename.chomp(".mp3").split(" - ")
   split_filename = filename.chop.chop.chop.chop.split(" - ")
   #first element is artist and second element is the song 
+  song = Song.new
+  song.name = split_filename[1]
+  song.artist_name = split_filename[0]
+  song
 end
 
-
+def 
 
 end 
 
